a633685ee678dfaa71d7b1db0fbf9bb1f56ab6b7
atharmon <atharmon@tamu.edu>
2022-09-15 00:26:11 -0500

    Updated theme and icon; added image for each Affirmation

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataSet = DataSource().loadAffirmations()
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, dataSet)
+        recyclerView.setHasFixedSize(true)
     }
 }
